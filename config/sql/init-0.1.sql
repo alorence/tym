549e58743858581973fe727f6bc02601f4c4d7a8
Antoine Lorence <antoine.lorence@gmail.com>
2012-11-14 08:31:11 +0100

    Update SQL init file

--- config/sql/init-0.1.sql
+++ config/sql/init-0.1.sql
@@ -9,10 +9,11 @@ INSERT INTO Infos (key, value) VALUES ('version', '0.1');
 CREATE TABLE Library (uid INTEGER PRIMARY KEY, filePath TEXT, bpid INTEGER REFERENCES BPTracks(bpid), status TEXT);
 
 # Main BeatPort infos tables
-CREATE TABLE BPTracks  (bpid INTEGER PRIMARY KEY, name TEXT, mixName TEXT, title TEXT, label INTEGER REFERENCES BPLabels(bpid), key TEXT, bpm TEXT, releaseDate INTEGER, publishDate INTEGER, price TEXT, length TEXT, release TEXT, imageUrl TEXT, imagePath TEXT);
+CREATE TABLE BPTracks  (bpid INTEGER PRIMARY KEY, name TEXT, mixName TEXT, title TEXT, label INTEGER REFERENCES BPLabels(bpid), key TEXT, bpm TEXT, releaseDate INTEGER, publishDate INTEGER, price TEXT, length TEXT, release TEXT, image REFERENCES BPTracksPictures(bpid));
 CREATE TABLE BPArtists (bpid INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE BPGenres  (bpid INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE BPLabels  (bpid INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE BPTracksPictures (bpid INTEGER PRIMARY KEY, url TEXT, localPath TEXT);
 
 # N-n join tables
 CREATE TABLE BPTracksArtistsLink (trackId INTEGER REFERENCES BPTracks(bpid), artistId INTEGER REFERENCES BPArtists(bpid), PRIMARY KEY(trackId, artistId));
