15d530c7f0b8ef163d0b511f6d2e58576778054f
Antoine Lorence <antoine.lorence@gmail.com>
2012-11-20 09:59:15 +0100

    Fix licence text

--- src/gui/settingsdialog.cpp
+++ src/gui/settingsdialog.cpp
@@ -3,18 +3,18 @@
 *
 * This file is part of TYM (Tag Your Music).
 *
-* TYM (Tag your Library) is free software: you can redistribute it and/or modify
+* TYM (Tag Your Music) is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
-* TYM (Tag your Library) is distributed in the hope that it will be useful,
+* TYM (Tag Your Music) is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with TYM (Tag your Library).  If not, see <http://www.gnu.org/licenses/>.
+* along with TYM (Tag Your Music).  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "settingsdialog.h"
