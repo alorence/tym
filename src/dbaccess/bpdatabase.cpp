412861cd144c8b8ade5c6dd0d914b0841041a2ed
Antoine Lorence <antoine.lorence@gmail.com>
2013-01-08 22:17:45 +0100

    (db) Store real UTF8 flat and shar chars

--- src/dbaccess/bpdatabase.cpp
+++ src/dbaccess/bpdatabase.cpp
@@ -336,10 +336,10 @@ QString BPDatabase::storeTrack(const QJsonValue track)
     QString key = stdKey.value("letter").toString();
     {
         if(stdKey.value("sharp").toBool()) {
-            key.append("#");
+            key.append("♯"); // ♯ is unicode char 266F, or L'\u266F' (in a c++ code)
         }
         if(stdKey.value("flat").toBool()) {
-            key.append("\u266D");
+            key.append('♭'); // ♭ is unicode char 266D, or L'\u266D' (in a c++ code)
         }
         key.append(" ");
         key.append(stdKey.value("chord").toString());
