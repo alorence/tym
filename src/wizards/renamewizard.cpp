alorence/tym

commit b6e8067a965d6b3b4d43f3def04c49a234ac93cd

(renamewizard) Add the PatternButton in gui

src/wizards/renamewizard.cpp
@@ -24,19 +24,25 @@ along with TYM (Tag Your Music). If not, see <http://www.gnu.org/licenses/>.
 #include "WidgetAppender.h"
 
 #include "commons.h"
-#include "tools/patterntool.h"
 #include "dbaccess/bpdatabase.h"
 #include "concretetasks/renametask.h"
 
 RenameWizard::RenameWizard(QList<QSqlRecord> selected, QWidget *parent) :
     QWizard(parent),
-    ui(new Ui::RenameWizard)
+    ui(new Ui::RenameWizard),
+    _filenameFormatter()
 {
     ui->setupUi(this);
+
+    _patternHelperButton = new PatternButton(_filenameFormatter, this);
 
     _widgetAppender = new WidgetAppender(ui->outputConsole);
     _widgetAppender->setFormat("%m\n");
     Logger::registerAppender(_widgetAppender);
+
+    ui->patternHorizLayout->addWidget(_patternHelperButton);
+    _patternHelperButton->hide();
+    connect(_patternHelperButton, SIGNAL(patternSelected(QString)), this, SLOT(insertPatternText(QString)));
 
     on_patternSelection_currentIndexChanged(ui->patternSelection->currentIndex());
 
@@ -92,8 +98,6 @@ RenameWizard::~RenameWizard()
 
 void RenameWizard::updateRenamePreview()
 {
-    FileBasenameFormatter filenameFormatter(ui->pattern->text());
-
     for(int row = 0 ; row < ui->previewTable->rowCount() ; ++row) {
         QString bpid = ui->previewTable->item(row, Bpid)->text();
 
@@ -101,7 +105,7 @@ void RenameWizard::updateRenamePreview()
 
         if( ! bpid.isEmpty()) {
             QFileInfo original(ui->previewTable->item(row, OrigFileName)->text());
-            QString newBaseName = filenameFormatter.format(_tracksInformations[bpid]);
+            QString newBaseName = _filenameFormatter.format(_tracksInformations[bpid]);
 
             if(newBaseName == original.baseName()) {
                 itemText = "<File already have the right name>";
@@ -114,6 +118,11 @@ void RenameWizard::updateRenamePreview()
         QTableWidgetItem *item = new QTableWidgetItem(itemText);
         ui->previewTable->setItem(row, TargetFileName, item);
     }
+}
+
+void RenameWizard::insertPatternText(const QString & patternText)
+{
+    ui->pattern->insert(patternText);
 }
 
 void RenameWizard::on_patternSelection_currentIndexChanged(int index)
@@ -130,11 +139,19 @@ void RenameWizard::on_patternSelection_currentIndexChanged(int index)
         break;
     }
     updateRenamePreview();
+
+    if(index == 3) { // Custom
+        _patternHelperButton->show();
+    } else {
+        _patternHelperButton->hide();
+    }
 }
 
 void RenameWizard::initializePage(int id)
 {
     if(id == ResultPage) {
+
+        _filenameFormatter.setPattern(ui->pattern->text());
 
         QHash<QString, QString> renameMap;
 

src/wizards/renamewizard.h
@@ -24,6 +24,9 @@ along with TYM (Tag Your Music). If not, see <http://www.gnu.org/licenses/>.
 #include <QtWidgets>
 #include <QtSql>
 
+#include "gui/patternbutton.h"
+#include "tools/patterntool.h"
+
 namespace Ui {
 class RenameWizard;
 }
@@ -43,11 +46,15 @@ protected:
     
 private slots:
     void updateRenamePreview();
+    void insertPatternText(const QString&);
     void on_patternSelection_currentIndexChanged(int index);
 
 private:
     Ui::RenameWizard* ui;
     QMap<QString, QSqlRecord> _tracksInformations;
+
+    FileBasenameFormatter _filenameFormatter;
+    PatternButton * _patternHelperButton;
 
     enum WizardPages {
         PreviewPage = 0,

src/wizards/renamewizard.ui
@@ -51,7 +51,7 @@
      </widget>
     </item>
     <item row="1" column="0">
-     <layout class="QHBoxLayout" name="horizontalLayout">
+     <layout class="QHBoxLayout" name="patternHorizLayout">
       <item>
        <widget class="QLabel" name="label">
         <property name="text">

commit c029e134d7bfbcf1aa41d6482ecb822ae35d2b1c

(renamewizard) Fix regression introduced in b6e8067a965d6b

src/wizards/renamewizard.cpp
@@ -98,6 +98,8 @@ RenameWizard::~RenameWizard()
 
 void RenameWizard::updateRenamePreview()
 {
+    _filenameFormatter.setPattern(ui->pattern->text());
+
     for(int row = 0 ; row < ui->previewTable->rowCount() ; ++row) {
         QString bpid = ui->previewTable->item(row, Bpid)->text();
 
@@ -150,8 +152,6 @@ void RenameWizard::on_patternSelection_currentIndexChanged(int index)
 void RenameWizard::initializePage(int id)
 {
     if(id == ResultPage) {
-
-        _filenameFormatter.setPattern(ui->pattern->text());
 
         QHash<QString, QString> renameMap;
 