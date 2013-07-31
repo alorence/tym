92ec6f07021f3ff6b15c10586516e9491f0dc34d
Antoine Lorence <antoine.lorence@gmail.com>
2013-08-01 00:06:30 +0200

    (resultDialog) First pass for new widget

---
+++ src/gui/taskmonitor.cpp
@@ -0,0 +1,46 @@
+/*****************************************************************************
+Copyright 2012-2013 Antoine Lorence. All right reserved.
+
+This file is part of TYM (Tag Your Music).
+
+TYM (Tag Your Music) is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TYM (Tag Your Music) is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TYM (Tag Your Music). If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+#include "taskmonitor.h"
+#include "ui_taskmonitor.h"
+
+TaskMonitor::TaskMonitor(Task *task, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::TaskMonitor),
+    _task(task)
+{
+    ui->setupUi(this);
+
+
+}
+
+TaskMonitor::~TaskMonitor()
+{
+    delete ui;
+}
+
+void TaskMonitor::updateCurrentState(const QString &state)
+{
+
+}
+
+void TaskMonitor::logEvent(const QString &key, TaskMonitor::EventType type, const QString &msg)
+{
+
+}
---
+++ src/gui/taskmonitor.h
@@ -0,0 +1,55 @@
+/*****************************************************************************
+Copyright 2012-2013 Antoine Lorence. All right reserved.
+
+This file is part of TYM (Tag Your Music).
+
+TYM (Tag Your Music) is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TYM (Tag Your Music) is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TYM (Tag Your Music). If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+#ifndef RESULTDIALOG_H
+#define RESULTDIALOG_H
+
+#include <QDialog>
+#include "concretetasks/task.h"
+
+namespace Ui {
+class TaskMonitor;
+}
+
+class TaskMonitor : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit TaskMonitor(Task * task, QWidget *parent = 0);
+    ~TaskMonitor();
+
+    enum EventType {
+        Info,
+        Warning,
+        Error,
+        Success
+    };
+
+public slots:
+    void updateCurrentState(const QString &state);
+    void logEvent(const QString &key, EventType type, const QString &msg);
+
+private:
+    Ui::TaskMonitor *ui;
+
+    Task * _task;
+};
+
+#endif // RESULTDIALOG_H
